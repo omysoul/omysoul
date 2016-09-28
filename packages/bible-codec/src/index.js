40aa8ec94b31843baf9caded34b800c6b6be362b
Phil Holden <phil.m.holden@gmail.com>
2016-09-28 14:31:47 +0100

    init

---
+++ packages/bible-codec/src/index.js
@@ -0,0 +1,23 @@
+export const encode = (bible) =>
+  bible.map(book =>
+    book.map(chapter =>
+      chapter.map(({ n, txt }) =>
+        `${n}|${txt}`
+      ).join('')
+    ).join('||')
+  ).join('|||')
+
+export const decode = (bible) =>
+  bible.split('|||').map(book =>
+    book.split('||').map(unflattenChapter)
+  )
+
+const unflattenChapter = chapter =>
+  chapter
+    .replace(/\d+\|/g, match => `||${match}`)
+    .substr(2)
+    .split('||')
+    .map(verse => {
+      const [n, txt] = verse.split('|')
+      return { n, txt }
+    })
