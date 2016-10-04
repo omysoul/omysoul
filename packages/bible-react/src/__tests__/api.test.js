9847104f3a6ba91fcba6dd3cbe5c5b8480766910
Phil Holden <phil.m.holden@gmail.com>
2016-10-04 22:17:54 +0100

    rename bible-react to react-bible

--- packages/bible-react/src/__tests__/api.test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-
-test('encode', t => {
-  t.true(
-    true
-  )
-})
--- packages/bible-react/src/index.js
+++
@@ -1,2 +0,0 @@
-export BibleVerseListPane from './components/bible-verse-list-pane'
-export BibleSearchPane from './components/bible-search-pane'
